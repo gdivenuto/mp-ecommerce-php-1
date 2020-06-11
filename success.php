f9183eca11862ca4a4b208f46fb6e2d791e17446
Gabriel Divenuto <37304699+gdivenuto@users.noreply.github.com>
2020-06-11 01:52:45 -0300

    Create success.php

---
+++ success.php
@@ -0,0 +1,10 @@
+<h1>Su pago se ha realizado con &eacute;xito</h1>
+
+<p>Collection Id: <?php echo $GET['collection_id']; ?></p>
+<p>Collection Status: <?php echo $GET['collection_status']; ?></p>
+<p>External Reference: <?php echo $GET['external_reference']; ?></p>
+<p>Payment Type: <?php echo $GET['payment_type']; ?></p>
+<p>Preference Id: <?php echo $GET['preference_id']; ?></p>
+<p>Site Id: <?php echo $GET['site_id']; ?></p>
+<p>Processing Mode: <?php echo $GET['processing_mode']; ?></p>
+<p>Merchant Account Id: <?php echo $GET['merchant_account_id']; ?></p>
