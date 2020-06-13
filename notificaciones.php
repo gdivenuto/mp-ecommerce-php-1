696f373cb92587061d8dde9bcb14a1ed01464ed2
Gabriel Divenuto <37304699+gdivenuto@users.noreply.github.com>
2020-06-13 15:20:01 -0300

    Update notificaciones.php

--- notificaciones.php
+++ notificaciones.php
@@ -1,4 +1,9 @@
 <?php
+/* PARA VER QUÉ INFORMACIÓN RECIBO DE MP */
+    echo '<pre>';
+    print_r($_GET);
+    echo '</pre>';
+/**/
     MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
     $merchant_order = null;
