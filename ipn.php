4facff524cc7c36e00a3074020bb524b61eb5160
Gabriel Divenuto <37304699+gdivenuto@users.noreply.github.com>
2020-06-14 00:35:49 -0300

    Update ipn.php

--- ipn.php
+++ ipn.php
@@ -1,24 +1,21 @@
- <?php
-/* PARA VER QUÉ INFORMACIÓN RECIBO DE Mercado Pago */
-    echo '<pre>';
-    print_r($_POST);
-    echo '</pre>';
-/**/
-    MercadoPago\SDK::setAccessToken("ENV_ACCESS_TOKEN");
+<?php
+MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
-    switch($_POST["type"]) {
-        case "payment":
-            $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
-            break;
-        case "plan":
-            $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
-            break;
-        case "subscription":
-            $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
-            break;
-        case "invoice":
-            $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
-            break;
-    }
+switch($_POST["type"]) {
+   case "payment":
+       $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+       break;
+   case "plan":
+       $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+       break;
+   case "subscription":
+       $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+       break;
+   case "invoice":
+       $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+       break;
+}
 
+header('Content-Type: application/json');
+echo json_encode(['HTTP/1.1 200 OK'], 200);
 ?>
