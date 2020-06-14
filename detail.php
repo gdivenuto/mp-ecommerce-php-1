f657d1cc56cfa3b49601c0414fb185bd6b076ad1
Gabriel Divenuto <37304699+gdivenuto@users.noreply.github.com>
2020-06-13 22:25:26 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -159,7 +159,7 @@ $preference->items = array($item);
 $preference->external_reference = "gabrieldivenuto@gmail.com";
 
 // URL a la cual es posible recibir notificaciones de pagos
-$preference->notification_url = "https://gdivenuto-mp-commerce-php.herokuapp.com/notificaciones";
+$preference->notification_url = "https://gdivenuto-mp-commerce-php.herokuapp.com/ipn";
 
 // Para redireccionar al comprador de nuevo a la Tienda
 // success: URL de retorno ante pago aprobado.
