9a5c04b2c632fac3b29d745e9be67b120d77cfb5
Gabriel Divenuto <37304699+gdivenuto@users.noreply.github.com>
2020-06-14 01:26:31 -0300

    Update ipn.php

--- ipn.php
+++ ipn.php
@@ -1,4 +1,6 @@
 <?php
+fputs(fopen("post.txt", 'w'), print_r($_POST, true));
+
 MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
 switch($_POST["type"]) {
