2cc57807b5777b34b0909e54fad1ff968d67ba54
Gabriel Divenuto <37304699+gdivenuto@users.noreply.github.com>
2020-06-14 00:36:43 -0300

    Update success.php

--- success.php
+++ success.php
@@ -1,10 +1,10 @@
 <h1>Su pago se ha realizado con &eacute;xito</h1>
 
-<p>Collection Id: <?php echo $GET['collection_id']; ?></p>
-<p>Collection Status: <?php echo $GET['collection_status']; ?></p>
-<p>External Reference: <?php echo $GET['external_reference']; ?></p>
-<p>Payment Type: <?php echo $GET['payment_type']; ?></p>
-<p>Preference Id: <?php echo $GET['preference_id']; ?></p>
-<p>Site Id: <?php echo $GET['site_id']; ?></p>
-<p>Processing Mode: <?php echo $GET['processing_mode']; ?></p>
-<p>Merchant Account Id: <?php echo $GET['merchant_account_id']; ?></p>
+<p>Collection Id: <?php echo $_GET['collection_id']; ?></p>
+<p>Collection Status: <?php echo $_GET['collection_status']; ?></p>
+<p>External Reference: <?php echo $_GET['external_reference']; ?></p>
+<p>Payment Type: <?php echo $_GET['payment_type']; ?></p>
+<p>Preference Id: <?php echo $_GET['preference_id']; ?></p>
+<p>Site Id: <?php echo $_GET['site_id']; ?></p>
+<p>Processing Mode: <?php echo $_GET['processing_mode']; ?></p>
+<p>Merchant Account Id: <?php echo $_GET['merchant_account_id']; ?></p>
