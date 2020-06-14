d2f08cfad60c416d337c9ff8a3c12673bfa89037
Gabriel Divenuto <37304699+gdivenuto@users.noreply.github.com>
2020-06-14 04:08:44 -0300

    Create webhook.php

---
+++ webhook.php
@@ -0,0 +1,21 @@
+<?php
+MercadoPago\SDK::setAccessToken("ENV_ACCESS_TOKEN");
+
+switch($_POST["type"]) {
+   case "payment":
+            $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+            break;
+   case "plan":
+            $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+            break;
+   case "subscription":
+            $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+            break;
+   case "invoice":
+            $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+            break;
+}
+
+header('Content-Type: application/json');
+echo json_encode(['HTTP/1.1 200 OK'], 200);
+?>
