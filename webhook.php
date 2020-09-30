926b8df4609b9643434588efe88497b61d622e0a
Gabriel Divenuto <37304699+gdivenuto@users.noreply.github.com>
2020-09-30 14:20:43 -0300

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,12 +1,11 @@
 <?php
 // Se obtiene el JSON
 $json = file_get_contents('php://input');
-// Se guarda en un archivo para leerlo
+// Se guarda en un archivo, para leerlo y tomar la info solicitada
 $filepost = "notificacion_webhook.json";
 file_put_contents($filepost, $json, FILE_APPEND);
-
+/**
 MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
-
 switch($_POST["type"]) {
    case "payment":
             $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
@@ -21,7 +20,7 @@ switch($_POST["type"]) {
             $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
             break;
 }
-
+/**/
 header('Content-Type: application/json');
 echo json_encode(['HTTP/1.1 200 OK'], 200);
 ?>
